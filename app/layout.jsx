9af97cbe16c6229e98186f38aafbeaafb75bd824
EmmtekDev <145601183+EmmtekDev@users.noreply.github.com>
2026-08-28 07:42:58 +0100

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,5 +1,38 @@
 import { Toaster } from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import StoreProvider from "@/app/StoreProvider";
+import Script from "next/script";
+import "./globals.css";
+
+export const metadata = {
+    title: "CraveStore — Where innovation meet elegance",
+    description: "Where innovation meet elegance",
+
+    other: {
+        "google-adsense-account": "ca-pub-1398515947251090",
+    },
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body className="antialiased">
+
+                <Script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1398515947251090"
+                    crossOrigin="anonymous"
+                />
+
+                <StoreProvider>
+                    <Toaster />
+                    {children}
+                </StoreProvider>
+
+            </body>
+        </html>
+    );
+}import StoreProvider from "@/app/StoreProvider";
 import "./globals.css";
 
 export const metadata = {
